2466a6431c3397ad93cfc4dfc9f71d57ebc197b5
HUANGLEI <3415796710@qq.com>
2019-11-05 17:51:26 +0800

    去除配置

--- config/config.default.js
+++ config/config.default.js
@@ -6,26 +6,4 @@
  * @property {String} SOME_KEY - some description
  */
 exports.grpcUtil = {
-  rpcServer: {
-    host: '0.0.0.0',
-    port: 50051,
-    autoConfig: 0,
-    protoPath: 'app/proto',
-    protoArray: [{
-      path: 'app/proto/login.proto',
-      packageService: 'login.login',
-      functionArray: ['login', 'signUp'],
-      pointFunArray: ['user.login', 'user.signUp'],
-    }],
-  },
-  rpcClient: {
-    host: '0.0.0.0',
-    port: 50051,
-    autoConfig: 0,
-    protoPath: 'app/proto',
-    protoArray: [{
-      path: 'app/proto/login.proto',
-      packageService: 'login.login',
-    }],
-  },
 };
